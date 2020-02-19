caabc0a053489501abdf041d7e76c19a0cd6ad53
habibsumoncse@gmail.com <habibsumoncse@gmail.com>
2020-02-19 11:10:20 -0800

    spring boot rest with audit

---
+++ spring-boot-audit-jpa/src/main/java/com/spring/rest/audit/AuditorAwareImpl.java
@@ -0,0 +1,14 @@
+package com.spring.rest.audit;
+
+import java.util.Optional;
+
+import org.springframework.data.domain.AuditorAware;
+
+public class AuditorAwareImpl implements AuditorAware<String> {
+
+	@Override
+	public Optional<String> getCurrentAuditor() {
+		return Optional.of("Ahasan");
+		// Use below commented code when will use Spring Security.
+	}
+}
